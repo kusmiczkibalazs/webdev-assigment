b533a3bef058d16d36bf90238d53d203890550bb
Kusmiczki Balázs <kusmiczkibalazs@gmail.com>
2021-12-03 22:43:17 +0100

    src/main/webapp/index.xhtml
    Added index.xhtml

--- src/main/java/hu/unideb/inf/Application.java
+++ src/main/java/hu/unideb/inf/Application.java
@@ -1,12 +1,35 @@
 package hu.unideb.inf;
 
+import com.sun.faces.config.ConfigureListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.Servlet;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public FacesServlet facesServlet() {
+        return new FacesServlet();
+    }
+
+    @Bean
+    public ServletRegistrationBean<Servlet> facesServletRegistration() {
+        ServletRegistrationBean<Servlet> registration = new ServletRegistrationBean<Servlet>(facesServlet(), "*.jsf");
+        return registration;
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean<ConfigureListener> jsfConfigureListener() {
+        return new ServletListenerRegistrationBean<ConfigureListener>(new ConfigureListener());
     }
 }
